650d50ce35a3336d5e3bc6da38a5816459a6d85e
TaiseiHamaya <146906391+TaiseiHamaya@users.noreply.github.com>
2024-06-17 10:17:37 +0900

    EN1_08_03まで

--- Assets/PullingActive.cs
+++ Assets/PullingActive.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PullingActive : MonoBehaviour {
 
 	Rigidbody rigid;
+	private Vector3 clickPosition;
+	private const float jumpPower = 10;
+	private bool canJump = false;
 
 	// Start is called before the first frame update
 	void Start() {
@@ -13,6 +16,27 @@ public class PullingActive : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			clickPosition = Input.mousePosition;
+		}
+		if (Input.GetMouseButtonUp(0) && canJump) {
+			Vector3 dist = clickPosition - Input.mousePosition;
+			if (dist.magnitude != 0) {
+				rigid.velocity = dist.normalized * jumpPower;
+			}
+		}
+	}
 
+	private void OnCollisionStay(Collision collision) {
+		ContactPoint[] contactPoint = collision.contacts;
+		Vector3 normal = contactPoint[0].normal;
+		float dot = Vector3.Dot(normal, Vector3.up);
+		if (dot >= Mathf.Cos(45)) {
+			canJump = true;
+		}
+	}
+
+	private void OnCollisionExit(Collision collision) {
+		canJump = false;
 	}
 }
