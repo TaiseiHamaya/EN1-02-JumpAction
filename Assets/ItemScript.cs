76777f25932a7b80fabd0c540fa385b4fdd11172
TaiseiHamaya <146906391+TaiseiHamaya@users.noreply.github.com>
2024-06-24 11:58:36 +0900

    EN1_09_02 EN1_09_03

--- Assets/ItemScript.cs
+++ Assets/ItemScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ItemScript : MonoBehaviour {
 
 	private Animator animator;
+	private AudioSource audioSource;
 
 	void DestroySelf() {
 		Destroy(gameObject);
@@ -13,6 +14,7 @@ public class ItemScript : MonoBehaviour {
 	// Start is called before the first frame update
 	void Start() {
 		animator = GetComponent<Animator>();
+		audioSource = GetComponent<AudioSource>();
 	}
 
 	// Update is called once per frame
@@ -22,5 +24,6 @@ public class ItemScript : MonoBehaviour {
 
 	private void OnTriggerEnter(Collider other) {
 		animator.SetTrigger("Get");
+		audioSource.Play();
 	}
 }
